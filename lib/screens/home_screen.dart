2aeb5835e42ee8d04906f0cdad24fea35a5ef3ac
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-28 11:11:40 +0200

    update home screen file & finish app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,77 +11,79 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final ApiService _apiService = ApiService();
-  bool _isLoading = false;
 
-  Future<void> _makeRequest(String url) async {
+  bool _isHttpLoading = false;
+  bool _isHttpsLoading = false;
+
+  Future<void> _makeRequest({required String url, required bool isHttp}) async {
     if (!mounted) return;
 
-    setState(() => _isLoading = true);
+    setState(() {
+      if (isHttp) {
+        _isHttpLoading = true;
+      } else {
+        _isHttpsLoading = true;
+      }
+    });
 
     final result = await _apiService.sendRequest(url);
 
     if (!mounted) return;
 
-    setState(() => _isLoading = false);
+    setState(() {
+      if (isHttp) {
+        _isHttpLoading = false;
+      } else {
+        _isHttpsLoading = false;
+      }
+    });
 
     Navigator.pushNamed(context, '/result', arguments: result);
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final width = size.width;
+    final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Proxy Me Please'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.info_outline),
-            onPressed: () => Navigator.pushNamed(context, '/about'),
+        backgroundColor: Color.fromARGB(255, 14, 142, 227),
+        elevation: 4,
+        title: const Text(
+          "Proxy Me Please",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white,
           ),
-        ],
+        ),
       ),
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.08),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(Icons.public, size: 80, color: Colors.blue),
-                  const SizedBox(height: 30),
-
-                  CustomButton(
-                    text: "HTTP Request",
-                    color: Colors.green,
-                    isLoading: _isLoading,
-                    onPressed: () {
-                      _makeRequest("http://httpbin.org/get");
-                    },
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  CustomButton(
-                    text: "HTTPS Request",
-                    color: Colors.orange,
-                    isLoading: _isLoading,
-                    onPressed: () {
-                      _makeRequest("https://httpbin.org/get");
-                    },
-                  ),
-
-                  const SizedBox(height: 30),
-
-                  const Text(
-                    "Tap a button to send request",
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
-                  ),
-                ],
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.08),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomButton(
+                text: "HTTP Request",
+                color: Colors.green,
+                isLoading: _isHttpLoading,
+                onPressed: () {
+                  _makeRequest(url: "http://httpbin.org/get", isHttp: true);
+                },
               ),
-            ),
+
+              const SizedBox(height: 20),
+
+              CustomButton(
+                text: "HTTPS Request",
+                color: Colors.orange,
+                isLoading: _isHttpsLoading,
+                onPressed: () {
+                  _makeRequest(url: "https://httpbin.org/get", isHttp: false);
+                },
+              ),
+            ],
           ),
         ),
       ),
